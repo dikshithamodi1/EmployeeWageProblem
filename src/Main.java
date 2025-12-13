7eab71f52131e6790f2a21798945ddc3127da396
Dikshitha <modidikshitha@gmail.com>
2025-12-13 21:58:45 +0530

    Initial push

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args){
+        //UC1
         System.out.println("Welcome to Employee Wage Computation Program");
 
         Employee e1 = new Employee();
 
-        e1.calculateDailyAttendance();  // determine if present/absent
+        e1.checkDailyAttendance();  // determine if present/absent
         System.out.println("Attendance value: " + e1.dailyAttendance);
 
-        int salary = e1.dailyWage();   // calculate wage
-        System.out.println("Daily wage is: " + salary);
+
     }
 }

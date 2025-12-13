3e584e72867bd02900a78c2a391489efe8cd3ef9
Dikshitha <modidikshitha@gmail.com>
2025-12-13 22:30:45 +0530

    switch statement added

--- src/Employee.java
+++ src/Employee.java
@@ -1,16 +1,22 @@
 public class Employee {
-    int absent=0;
-    int present=1;
+    static final int absent=0;
+    static final int present=1;
     int dailyAttendance;
     int hourlyWage=20;
     int fullDay=8;
     int dailyWage;
+    static final int partTime=2;
+    int partTimeHours=4;
     public void checkDailyAttendance(){
-        int empcheck=(int)(Math.random()*2);
+        int empcheck=(int)(Math.random()*3);
         if (empcheck==present){
             dailyAttendance=present;
             System.out.println("Employee is present");
-        }else{
+        } else if (empcheck==partTime) {
+            dailyAttendance=partTime;
+            System.out.println("Employee is parttime");
+        }
+        else{
             dailyAttendance=absent;
             System.out.println("Employee is not present");
         }
@@ -19,9 +25,27 @@ public class Employee {
         if (dailyAttendance==present){
             dailyWage=hourlyWage*fullDay;
             System.out.println("Daily wage: " + dailyWage);
-        }else{
+            //UC3
+        } else if (dailyAttendance==partTime) {
+            dailyWage=partTimeHours*hourlyWage;
+            System.out.println("Daily wage: " + dailyWage);
+        } else{
             dailyWage=0;
             System.out.println("No Pay");
         }
+    }
+    // UC4: Calculate Daily Wage using Switch Case
+    public void calculateDailyWageUsingSwitch() {
+        switch (dailyAttendance) {
+            case present:
+                dailyWage = hourlyWage * fullDay;
+                break;
+            case partTime:
+                dailyWage = hourlyWage * partTimeHours;
+                break;
+            default:
+                dailyWage = 0;
+        }
+        System.out.println("Daily Wage using switch: " + dailyWage);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Attendance value: " + e1.dailyAttendance);
 
         e1.calculateDailyWage();
+        e1.calculateDailyWageUsingSwitch();
     }
 }
